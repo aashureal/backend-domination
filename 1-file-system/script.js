1ecabd5fe358baaac2bc9acbdd6045f7a73a76a6
Aashutosh Uikey <aashuuy09@gmail.com>
2025-04-17 15:05:55 +0530

    Folders

--- 1-file-system/script.js
+++ 1-file-system/script.js
@@ -1,31 +1,57 @@
 const fs = require("fs");
 
+// **************************
+//          Files
+// **************************
+
 // 1. Write File
-// fs.writeFile("./files/f3.txt", "This is my third note.", (err) => {
-//   if (err) throw err;
-//   console.log("File created and written!");
-// });
+fs.writeFile("./files/f3.txt", "This is my third note.", (err) => {
+  if (err) throw err;
+  console.log("File created and written!");
+});
 
 // Append File
-// fs.appendFile("./files/f1.txt", "\nAdding one more line.", (err) => {
-//   if (err) throw err;
-//   console.log("Content added to file!");
-// });
+fs.appendFile("./files/f1.txt", "\nAdding one more line.", (err) => {
+  if (err) throw err;
+  console.log("Content added to file!");
+});
 
 // Read File
-// fs.readFile("./files/f1.txt", "utf8", (err, data) => {
-//   if (err) throw err;
-//   console.log(data);
-// });
+fs.readFile("./files/f1.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
 
 // Rename File
-// fs.rename("./files/f2.txt", "newName.txt", (err) => {
-//   if (err) throw err;
-//   console.log("File Deleted!");
-// });
+fs.rename("./files/f2.txt", "newName.txt", (err) => {
+  if (err) throw err;
+  console.log("File Deleted!");
+});
 
 // Delete File
-// fs.unlink("./files/f3.txt", (err) => {
-//   if (err) throw err;
-//   console.log("File deleted.");
-// });
+fs.unlink("./files/f3.txt", (err) => {
+  if (err) throw err;
+  console.log("File deleted.");
+});
+
+// **************************
+//          Folder
+// **************************
+
+// 1. Folder Creation
+fs.mkdir("./folder2", (err) => {
+  if (err) throw err;
+  console.log("Folder Created!");
+});
+
+// 2. Read Folder
+fs.readdir("./myFolder", (err, files) => {
+  if (err) throw err;
+  console.log(`Folder Content ${files}`);
+});
+
+// 3. Delete Folder
+fs.rmdir("folder2", (err) => {
+  if (err) throw err;
+  console.log("Folder deleted!");
+});
