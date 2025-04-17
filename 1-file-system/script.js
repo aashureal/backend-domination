44f757f0696af3c0bb07c4de8691f515725061e6
Aashutosh Uikey <aashuuy09@gmail.com>
2025-04-17 14:53:08 +0530

    file crud

--- 1-file-system/script.js
+++ 1-file-system/script.js
@@ -1,20 +1,31 @@
 const fs = require("fs");
 
-// update file
-// append file
-// folder creation
-
-// write file
-// fs.writeFile("efgh.txt", "Another File Created.", (err) => {
-  //   if (err) throw err;
-//   console.log("File has been saved.");
+// 1. Write File
+// fs.writeFile("./files/f3.txt", "This is my third note.", (err) => {
+//   if (err) throw err;
+//   console.log("File created and written!");
 // });
 
+// Append File
+// fs.appendFile("./files/f1.txt", "\nAdding one more line.", (err) => {
+//   if (err) throw err;
+//   console.log("Content added to file!");
+// });
 
+// Read File
+// fs.readFile("./files/f1.txt", "utf8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
 
+// Rename File
+// fs.rename("./files/f2.txt", "newName.txt", (err) => {
+//   if (err) throw err;
+//   console.log("File Deleted!");
+// });
 
-// read file
-fs.readFile('abcd.txt', 'utf8', (err, data)=>{
-  if(err) throw err;
-  console.log(data);
-})
+// Delete File
+// fs.unlink("./files/f3.txt", (err) => {
+//   if (err) throw err;
+//   console.log("File deleted.");
+// });
